f7cac43dc5e703cfa970c22a0c64f2663a4de000
cycycy <galoncyryll@gmail.com>
2018-12-31 13:47:33 -0800

    change database location

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const img = require('./controllers/img');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-encircled-23773',
       user : 'postgres',
       password : 'test',
       database : 'smartbrain'
