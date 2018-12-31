ad3aed1fbc25e0eefeb825a7d9877e7204a3141c
cycycy <galoncyryll@gmail.com>
2018-12-31 13:56:25 -0800

    updating database url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const img = require('./controllers/img');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl : true
     }
 });
