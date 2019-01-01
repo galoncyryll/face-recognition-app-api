9a1d3569a4db011d23395ec12f1bc15e771d8e34
cycycy <galoncyryll@gmail.com>
2019-01-01 12:13:04 -0800

    reverting changes

--- controllers/img.js
+++ controllers/img.js
@@ -1,16 +1,16 @@
 const Clarifai = require('clarifai');
 
-// const app = new Clarifai.App({
-//     apiKey: '7833de21626943dea74b9ffda69ab497'
-// });
+const app = new Clarifai.App({
+    apiKey: '7833de21626943dea74b9ffda69ab497'
+});
 
-// const handleApiCall = (req, res) => {
-//     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-//     .then(data => {
-//         res.json(data);
-//     })
-//     .catch(err => res.status(400).json('unable to work with API'));
-// }
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+}
 
 const handleImg = (req, res, db) => {
     const { id } = req.body;
