51650cca5d33b5f347ae8ca994096281a9edcbee
cycycy <galoncyryll@gmail.com>
2019-01-17 15:36:23 -0800

    test changes

--- controllers/img.js
+++ controllers/img.js
@@ -9,7 +9,7 @@ const handleApiCall = (req, res) => {
     .then(data => {
         res.json(data);
     })
-    .catch(err => res.status(400).json('unable to connect to API'));
+    .catch(err => res.status(400).json(err));
 }
 
 const handleImg = (req, res, db) => {
