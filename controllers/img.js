a8d82eefc44f470bc1d5f508a490e7184778fdf8
cycycy <galoncyryll@gmail.com>
2019-01-17 15:27:11 -0800

    test change

--- controllers/img.js
+++ controllers/img.js
@@ -5,11 +5,11 @@ const app = new Clarifai.App({
 });
 
 const handleApiCall = (req, res) => {
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg")
     .then(data => {
         res.json(data);
     })
-    .catch(err => res.status(400).json('unable to connect to API'));
+    .catch(err => res.status(400).json('unable to connect to API test'));
 }
 
 const handleImg = (req, res, db) => {
